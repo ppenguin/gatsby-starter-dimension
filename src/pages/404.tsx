d1856f97f39cea3ab494fb8fed6fe61a86766c66
ppenguin <hieronymusv@gmail.com>
2021-03-21 12:08:31 +0100

    port to ts and update all deps to latest

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,9 @@
+import { PageProps } from 'gatsby'
 import React from 'react'
 import Layout from '../components/layout'
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = (props: PageProps) => (
+  <Layout {...props}> {/* pass props because in Layout {children,location} are used */}
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
